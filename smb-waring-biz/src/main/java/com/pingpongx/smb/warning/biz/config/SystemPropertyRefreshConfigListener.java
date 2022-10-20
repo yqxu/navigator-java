d40f419a1acdf75292cf12319763748ca1b94394
徐杨清 <xuyq@pingpongx.com>
2022-10-20 18:40:02 +0800

    configured inhibition supported.

---
+++ smb-waring-biz/src/main/java/com/pingpongx/smb/warning/biz/config/SystemPropertyRefreshConfigListener.java
@@ -0,0 +1,19 @@
+package com.pingpongx.smb.warning.biz.config;
+
+import com.pingpongx.config.client.listener.ConfigListener;
+import com.pingpongx.smb.warning.biz.moudle.Trie;
+import com.pingpongx.smb.warning.biz.rules.Rule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+//@Slf4j
+//public class InhibitionConfigListener implements ConfigListener {
+//
+//    @Override
+//    public void onChange(String key, String value) {
+//        if(key.equals("configured.scene")){
+//
+//        }
+//
+//    }
+//}
