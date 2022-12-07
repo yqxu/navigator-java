23c3260cd19716a9e50549dbf13ead4202e669d2
J <jianggm@jianggm-1.local>
2022-12-07 16:38:39 +0800

    恢复

--- smb-warning-web/src/main/java/com/pingpongx/smb/warning/web/controller/WebhookController.java
+++ smb-warning-web/src/main/java/com/pingpongx/smb/warning/web/controller/WebhookController.java
@@ -6,12 +6,14 @@ import com.pingpongx.flowmore.cloud.base.server.annotation.NoAuth;
 import com.pingpongx.smb.warning.api.dto.JiraDTO;
 import com.pingpongx.smb.warning.biz.service.DingTalkRobotsService;
 import com.pingpongx.smb.warning.biz.util.JiraUtils;
-import com.pingpongx.smb.warning.web.dao.JiraInfoDao;
 import com.pingpongx.smb.warning.web.helper.BusinessAlertHelper;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Auther: jiangkun
@@ -27,9 +29,8 @@ public class WebhookController {
 
     private final BusinessAlertHelper businessAlertHelper;
 
-    private final DingTalkRobotsService dingTalkRobotsService;
+    private final DingTalkRobotsService  dingTalkRobotsService;
 
-    private final JiraInfoDao jiraInfoDao;
     @ApiOperation("jira-消息回调通知")
     @PostMapping("/jirahook")
     @NoAuth(isPack = false)
@@ -50,26 +51,6 @@ public class WebhookController {
     public void robotsCallBack(@RequestBody(required = false) JSONObject json) {
         log.info("{}", JSON.toJSONString(json));
         dingTalkRobotsService.responseDingTalk(json);
-    }
-
-    @ApiOperation("客户服务-消息回调通知")
-    @NoAuth(isPack = false)
-    @PostMapping(value = "/robots/dingtalk/customer/service")
-    public void customerServiceRobotsCallBack(@RequestBody(required = false) JSONObject json) {
-        log.info("客户服务-消息回调内容 {}", JSON.toJSONString(json));
-    }
-
-    @ApiOperation("jira-消息回调通知")
-    @PostMapping("/jira/hook/customer/service")
-    @NoAuth(isPack = false)
-    public void customerServiceJiraHook(@RequestBody String message,
-                                        @RequestParam("issueId") String issueId,
-                                        @RequestParam("issueKey") String issueKey,
-                                        @RequestParam("projectId") String projectId,
-                                        @RequestParam("projectKey") String projectKey) {
-        log.info("客户服务-jira消息回调内容 message = {}, issueId= {}, issueKey = {}, projectId={}, projectKey ={}", message, issueId, issueKey, projectId, projectKey);
-        JiraDTO jiraDTO = JiraUtils.parseJiraDTO(message);
-        jiraInfoDao.updateStatus(jiraDTO.getStatus(), issueId, projectKey);
     }
 
 
