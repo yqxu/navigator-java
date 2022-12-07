a839da4d1695f63a1218311456fa6b337854da9d
J <jianggm@jianggm-1.local>
2022-12-07 17:35:17 +0800

    增加jiralog

--- smb-warning-web/src/main/java/com/pingpongx/smb/warning/web/controller/WebhookControllerV1.java
+++ smb-warning-web/src/main/java/com/pingpongx/smb/warning/web/controller/WebhookControllerV1.java
@@ -2,15 +2,18 @@ package com.pingpongx.smb.warning.web.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.pingpongx.flowmore.cloud.base.commom.utils.PPConverter;
 import com.pingpongx.flowmore.cloud.base.server.annotation.NoAuth;
-import com.pingpongx.smb.warning.api.dto.JiraDTO;
-import com.pingpongx.smb.warning.biz.util.JiraUtils;
 import com.pingpongx.smb.warning.web.constant.WarnConstants;
 import com.pingpongx.smb.warning.web.dao.JiraInfoDao;
+import com.pingpongx.smb.warning.web.module.JiraChangelog;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @Auther: jiangkun
@@ -40,13 +43,23 @@ public class WebhookControllerV1 {
                                         @RequestParam("issueKey") String issueKey,
                                         @RequestParam("projectId") String projectId,
                                         @RequestParam("projectKey") String projectKey) {
-        log.info("客户服务-jira消息回调内容 message = {}, issueId= {}, issueKey = {}, projectId={}, projectKey ={}", message, issueId, issueKey, projectId, projectKey);
+        log.info("客户服务-jira消息回调内容 issueId= {}, issueKey = {}, projectId={}, projectKey ={}", issueId, issueKey, projectId, projectKey);
         if (!"RECALL".equals(projectKey)) {
             return;
         }
-        JiraDTO jiraDTO = JiraUtils.parseJiraDTO(message);
-        jiraInfoDao.updateStatus(jiraDTO.getStatus(), issueId, projectKey);
-    }
+        Map<String, Object> stringObjectMap = PPConverter.toObject(message, new TypeReference<JSONObject>() {
+        });
+        JiraChangelog jiraChangelog = PPConverter.toObject(PPConverter.toJsonString(stringObjectMap.get("changelog")), JiraChangelog.class);
+        log.info("jiraChangelog = {}", PPConverter.toJsonStringIgnoreException(jiraChangelog));
+        if (jiraChangelog == null || jiraChangelog.getItems() == null || jiraChangelog.getItems().isEmpty()) {
+            return;
+        }
+        for (JiraChangelog.Item item : jiraChangelog.getItems()) {
+            if ("status".equals(item.getField())) {
 
+                jiraInfoDao.updateStatus(item.getToString(), issueId, projectKey);
+            }
+        }
+    }
 
 }
